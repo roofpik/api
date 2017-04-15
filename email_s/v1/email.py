997c61b02b2c0b18b7263f16c1c58acee76715c9
Arpit Mittal <arpit@roofpik.com>
2017-04-15 18:44:13 +0530

    encoding added for email

--- email_s/v1/email.py
+++ email_s/v1/email.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import request
 from restful import Resource
 from flask_restful import reqparse
--- email_s/v1/templates/welcome.py
+++ email_s/v1/templates/welcome.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from string import Template
 
 def welcomeTemplate(name):
